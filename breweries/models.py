4abc37bb8dcfda54e73a1239329390a51be88abf
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-01-27 11:48:58 -0500

    about to set up REST

--- breweries/models.py
+++ breweries/models.py
@@ -7,18 +7,27 @@ class Brewery(models.Model):
     hours = models.CharField(max_length=20)
     logo_url = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Food(models.Model):
     item = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     price = models.CharField(max_length=20)
     brewery = models.ForeignKey(Brewery, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.item
+
 class Beer(models.Model):
     name = models.CharField(max_length=100)
     style = models.CharField(max_length=100)
-    description = models.CharField(max_length=255)
+    abv = models.CharField(max_length=10)
     full_price = models.CharField(max_length=10)
     flight_price = models.CharField(max_length=10)
     glassware = models.CharField(max_length=20)
-    abv = models.TextField(max_length=10)
+    description = models.TextField(max_length=255)
     brewery = models.ForeignKey(Brewery, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name

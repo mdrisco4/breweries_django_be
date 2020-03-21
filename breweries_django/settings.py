73261b6c05e86399b3a2df3c56c3cd9710e0758b
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-03-20 20:18:07 -0400

    solved nonresponding styling issue

--- breweries_django/settings.py
+++ breweries_django/settings.py
@@ -83,7 +83,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
+    # 'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'breweries_django.urls'

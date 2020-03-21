2f3ae1d684e3ce6527dd2da5a6284a769e3222ac
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-03-20 20:02:28 -0400

    livereload installed

--- breweries_django/settings.py
+++ breweries_django/settings.py
@@ -83,6 +83,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'breweries_django.urls'
